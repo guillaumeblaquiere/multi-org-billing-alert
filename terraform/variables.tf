1a59ba58a9e7b53a1a6b86de6d80e3fa1d733687
Renaud BUTTIERO <renaud.buttiero.ext@lvmh.com>
2023-01-23 10:53:49 +0100

    Add variable types

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,16 @@
 variable "billing_project" {
   description = "The project that will host the resources (service account, pubsub, cloud run)"
+  type        = string
 }
 
 variable "runtime_project" {
   description = "The project that will host the notification channel email (for sending emails)"
+  type        = string
 }
 
 variable "billing_account" {
   description = "The billing account on which to create the budget alerts"
+  type        = string
 }
 
 variable "members" {
@@ -17,25 +20,31 @@ variable "members" {
 }
 
 variable "image_tag" {
+  type    = string
   default = "us-central1-docker.pkg.dev/gblaquiere-dev/public/multi-org-billing-alert:1.4"
 }
 
 variable "region" {
+  type    = string
   default = "us-central1"
 }
 
 variable "service_account_name" {
+  type    = string
   default = "multi(org-billing"
 }
 
 variable "cloud_run_service_name" {
+  type    = string
   default = "multi-org-billing-alert"
 }
 
 variable "pubsub_topic_name" {
+  type    = string
   default = "multi-org-billing-alert-topic"
 }
 
 variable "pubsub_subscription_name" {
+  type    = string
   default = "multi-org-billing-alert-subscription"
 }
